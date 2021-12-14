a0ea3974fc1686002ccd49a65fca0cd6ab2aaeae
hpsure <719674268@qq.com>
2021-12-14 22:56:16 +0800

    init

---
+++ gulimall-order/src/test/java/com/hp/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hp.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
